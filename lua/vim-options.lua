03f1453d19f7a68d06942a9c21de4b8b266c1f2a
craigesmith <craigelliss1993@gmail.com>
2024-04-01 11:26:16 +0000

    starting config for neovim

---
+++ lua/vim-options.lua
@@ -0,0 +1,21 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+vim.g.background = "dark"
+
+vim.opt.swapfile = false
+vim.o.termguicolors = true
+
+--vim.o.background = 'dark'
+
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.wo.number = true
